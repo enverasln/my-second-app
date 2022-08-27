0fc353e655bf8d47279611da2fcec1d2775c52dd
enverasln <enver.asln@gmail.com>
2022-08-27 16:38:08 +0300

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/add-form/add-form.component.html 	new file:   src/app/add-form/add-form.component.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/error/error.component.ts 	new file:   src/app/service.component.html 	new file:   src/app/service.component.ts 	new file:   src/app/success/success.component.html 	new file:   src/app/success/success.component.scss 	new file:   src/app/success/success.component.spec.ts 	new file:   src/app/success/success.component.ts

---
+++ src/app/add-form/add-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-add-form',
+    template: '<p>Bu bir deneme HTML</p>'
+    //templateUrl: './add-form.component.html'
+})
+export class AddFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AddFormComponent } from './add-form/add-form.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { ServiceComponent } from './service.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    AddFormComponent,
+    SuccessComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
